78becaa81b7e262f2273e21a64ece33547be09b7
Alexandre <alexandre1alves@gmail.com>
2026-02-06 23:36:08 -0300

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-2">
+        <img src="./public/Logo.png" alt="Logo" className="h-15 w-15" />
+        <p className=""> AOA - Dev Hotéis </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
