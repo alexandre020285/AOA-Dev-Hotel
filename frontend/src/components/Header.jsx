b5c549dd3ba55d2313927c1d9fd6048f820b3bab
Alexandre <alexandre1alves@gmail.com>
2026-02-06 23:53:47 -0300

    feat: criado o header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <div className="flex items-center gap-2">
-        <img src="./public/Logo.png" alt="Logo" className="h-15 w-15" />
-        <p className=""> AOA - Dev Hotéis </p>
+    <div className="shadow-md">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-8 py-4">
+        <div className="flex items-center gap-2">
+          <img src="./public/Logo.png" alt="Logo" className="h-10" />
+          <p className=""> AOA - Dev Hotéis </p>
+        </div>
+        <div> Procura</div>
+        <div>Perfil</div>
       </div>
     </div>
   );
